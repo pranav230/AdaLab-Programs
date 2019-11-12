b78fa942eff6ffe8265f5984e278bb422b25909a
Pranav <pranavbansal777@gmail.com>
2019-11-12 19:52:27 +0530

    Edited mergeSort

--- Divide And Conquer/mergeSort.c
+++ Divide And Conquer/mergeSort.c
@@ -4,53 +4,31 @@
   
 // Merges two subarrays of arr[]. 
 void merge(int arr[], int l, int m, int r) 
-{ 
-    int i, j, k; 
-    int n1 = m - l + 1; 
-    int n2 =  r - m; 
-  
-    int L[n1], R[n2]; 
-  
-    // Copy data to temp arrays L[] and R[]
-    for (i = 0; i < n1; i++) 
-        L[i] = arr[l + i]; 
-    for (j = 0; j < n2; j++) 
-        R[j] = arr[m + 1+ j]; 
-  
-    // Merge the temp arrays back into arr[l..r]
-    i = 0; 		// Initial index of first subarray 
-    j = 0; 		// Initial index of second subarray 
-    k = l; 		// Initial index of merged subarray 
-    while (i < n1 && j < n2) 
-    { 
-        if (L[i] <= R[j]) 
-        { 
-            arr[k] = L[i]; 
-            i++; 
-        } 
-        else
-        { 
-            arr[k] = R[j]; 
-            j++; 
-        } 
-        k++; 
-    } 
-  
-    // Copy the remaining elements of L[], if there are any
-    while (i < n1) 
-    { 
-        arr[k] = L[i]; 
-        i++; 
-        k++; 
-    } 
-  
-    // Copy the remaining elements of R[], if there are any 
-    while (j < n2) 
-    { 
-        arr[k] = R[j]; 
-        j++; 
-        k++; 
-    } 
+{
+	//b is the temporary array
+    int i,k,j,b[500000];
+	i=k=low;
+	j=mid+1;
+
+	while(i<=mid && j<=high)
+	{
+		if(arr[i]<arr[j])
+			b[k++]=arr[i++];
+		else
+			b[k++]=arr[j++];
+	}
+	
+	// Copy the remaining elements of L[], if there are any
+	while(i<=mid)
+		b[k++]=arr[i++];
+	
+	// Copy the remaining elements of R[], if there are any 
+	while(j<=high)
+		b[k++]=arr[j++];
+
+	//copy the elements back to the original array
+	for(i=low;i<k;i++)
+		arr[i]=b[i];
 } 
   
 void mergeSort(int arr[], int l, int r) 
@@ -62,7 +40,7 @@ void mergeSort(int arr[], int l, int r)
         // Sort first and second halves 
         mergeSort(arr,l,m); 
         mergeSort(arr,m+1,r); 
-  
+
         merge(arr, l, m, r); 
     } 
 } 
